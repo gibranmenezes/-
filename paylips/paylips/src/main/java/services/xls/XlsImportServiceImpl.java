86aa9bda6ad88832e699aca13f9a61da93fd4fee
gibranmenezes <gibranquimica@gmail.com>
2023-07-20 13:38:26 -0300

    corrections

--- paylips/paylips/src/main/java/services/xls/XlsImportServiceImpl.java
+++ paylips/paylips/src/main/java/services/xls/XlsImportServiceImpl.java
@@ -14,7 +14,7 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.ss.usermodel.*;
 import repositories.*;
 
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
